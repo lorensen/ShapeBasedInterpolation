025fb07a2ab858dbb48382129b3c444172f3f779
Bill Lorensen <bill.lorensen@gmail.com>
2013-12-29 15:19:17 -0500

    ENH: ITK style

--- include/itkIntraBinaryShapeBasedInterpolationImageFilter.h
+++ include/itkIntraBinaryShapeBasedInterpolationImageFilter.h
@@ -1,4 +1,4 @@
-/*===============================================================================================================
+/*===========================================================================
   Program:    Insight Segmentation & Registration Toolkit
   Language:   C++
   Date:       june 2012
@@ -15,10 +15,10 @@
   This software is distributed WITHOUT ANY WARRANTY; without even the implied 
   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
   See the copyright notices for more information.
-================================================================================================================*/
+=============================================================================*/
 
-#ifndef __INTRABINARYSHAPEBASEDINTERPOLATIONIMAGEFILTER_H
-#define __INTRABINARYSHAPEBASEDINTERPOLATIONIMAGEFILTER_H
+#ifndef __itkIntraBinaryShapeBasedInterpolationImageFilter_h
+#define __itkIntraBinaryShapeBasedInterpolationImageFilter_h
 
 #include <itkImageToImageFilter.h>
 #include <itkImage.h>
@@ -36,139 +36,147 @@
 
 namespace itk
 {
-  /** \class IntraBinaryShapeBasedInterpolationImageFilter
+/** \class IntraBinaryShapeBasedInterpolationImageFilter
   * \brief Interpolates  a 3D segmented binary image wherein all slices are segmented.
-  * This filter takes as input a 3D binary image wherein the object of interest is represented in full, 
-  * that is, on all contiguous slices that comprise it. Such filter yields an upsampled 3D binary image 
+  * This filter takes as input a 3D binary image wherein the object of
+  * interest is represented in full, that is, on all contiguous slices
+  * that comprise it. Such filter yields an upsampled 3D binary image
   * with thinner delineated contours.
-  * Note that the DelineationRatio defined as the ratio of the delineated slices in the output image 
-  * (i.e. after interpolation) to the number of the delineated slices in the input image (i.e. before 
-  * interpolation) needs to be set up.
-  * Reference paper : S.P. Raya and J.K. Udupa. Shape-based interpolation of multidimensional objects. 1990
+  * Note that the DelineationRatio defined as the ratio of the
+  * delineated slices in the output image (i.e. after interpolation)
+  * to the number of the delineated slices in the input image
+  * (i.e. before interpolation) needs to be set up.
+  * Reference paper : S.P. Raya and J.K. Udupa. Shape-based
+  * interpolation of multidimensional objects. 1990
   */
 
-  class ITK_EXPORT IntraBinaryShapeBasedInterpolationImageFilter : 
-      public itk::ImageToImageFilter < itk::Image< unsigned char, 3 >, itk::Image< unsigned char, 3 > >
-  {   
-  public:           
-    typedef unsigned char InputPixelType;
-    typedef double IntermediatePixelType;
+class ITK_EXPORT IntraBinaryShapeBasedInterpolationImageFilter : 
+    public itk::ImageToImageFilter < itk::Image< unsigned char, 3 >, itk::Image< unsigned char, 3 > >
+{   
+public:           
+  typedef unsigned char InputPixelType;
+  typedef double        IntermediatePixelType;
     
-    typedef itk::Image< InputPixelType, 3 >                           InputImageType;
-    typedef itk::Image< InputPixelType, 3 >                           IntermediateImageType;
-    typedef itk::Image< IntermediatePixelType, 3 >                    IntermediateImageType2;
-    typedef itk::Image< InputPixelType, 2 >                           InternalInImageType;
-    typedef itk::Image< IntermediatePixelType, 2 >                    InternalOutImageType;
-    typedef itk::Image< InputPixelType, 3 >                           OutputImageType;
+  typedef itk::Image< InputPixelType, 3 >        InputImageType;
+  typedef itk::Image< InputPixelType, 3 >        IntermediateImageType;
+  typedef itk::Image< IntermediatePixelType, 3 > IntermediateImageType2;
+  typedef itk::Image< InputPixelType, 2 >        InternalInImageType;
+  typedef itk::Image< IntermediatePixelType, 2 > InternalOutImageType;
+  typedef itk::Image< InputPixelType, 3 >        OutputImageType;
 
-    /** Standard class typedefs used for object creation with the object factory. */
-    typedef IntraBinaryShapeBasedInterpolationImageFilter             Self;
-    typedef itk::ImageToImageFilter<InputImageType, OutputImageType>  Superclass;
-    typedef itk::SmartPointer<Self>                                   Pointer;
-    typedef itk::SmartPointer<const Self>                             ConstPointer;
+  /** Standard class typedefs used for object creation with the object
+   * factory.
+   */
+  typedef IntraBinaryShapeBasedInterpolationImageFilter             Self;
+  typedef itk::ImageToImageFilter<InputImageType, OutputImageType>  Superclass;
+  typedef itk::SmartPointer<Self>                                   Pointer;
+  typedef itk::SmartPointer<const Self>                             ConstPointer;
     
-    /** Method for creation through the object factory. */
-    itkNewMacro(Self);
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
 
-    /** Run-time type information (and related methods). */
-    itkTypeMacro( IntraBinaryShapeBasedInterpolationImageFilter, ImageToImageFilter );    
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( IntraBinaryShapeBasedInterpolationImageFilter, ImageToImageFilter );    
 
-    /** Typedefs to access images. */
-    typedef itk::ImageSliceConstIteratorWithIndex< InputImageType >   ConstSliceItType;
-    typedef itk::ImageRegionConstIterator< InputImageType >           ConstRegionItType;
-    typedef itk::ImageRegionIterator< InputImageType >                RegionItType;
-    typedef itk::ImageRegionIterator< IntermediateImageType2 >        IntermediateRegionItType;
-    typedef itk::ImageRegionIterator< OutputImageType >               OutputRegionItType;
+  /** Typedefs to access images. */
+  typedef itk::ImageSliceConstIteratorWithIndex< InputImageType >   ConstSliceItType;
+  typedef itk::ImageRegionConstIterator< InputImageType >           ConstRegionItType;
+  typedef itk::ImageRegionIterator< InputImageType >                RegionItType;
+  typedef itk::ImageRegionIterator< IntermediateImageType2 >        IntermediateRegionItType;
+  typedef itk::ImageRegionIterator< OutputImageType >               OutputRegionItType;
 
-    typedef InputImageType::Pointer                          InputImagePointer; 
-    typedef InputImageType::ConstPointer                     InputImageConstPointer; 
-    typedef InputImageType::RegionType                       InputRegionType; 
-    typedef InputImageType::SizeType                         InputSizeType;
-    typedef InputImageType::SpacingType                      InputSpacingType; 
-    typedef InputImageType::IndexType                        InputIndexType;
-    typedef InputImageType::PointType                        InputPointType;
+  typedef InputImageType::Pointer                          InputImagePointer; 
+  typedef InputImageType::ConstPointer                     InputImageConstPointer; 
+  typedef InputImageType::RegionType                       InputRegionType; 
+  typedef InputImageType::SizeType                         InputSizeType;
+  typedef InputImageType::SpacingType                      InputSpacingType; 
+  typedef InputImageType::IndexType                        InputIndexType;
+  typedef InputImageType::PointType                        InputPointType;
     
-    typedef IntermediateImageType::Pointer                   IntermediateImagePointer;
-    typedef IntermediateImageType::RegionType                IntermediateRegionType;
-    typedef IntermediateImageType::SizeType                  IntermediateSizeType; 
-    typedef IntermediateImageType::SpacingType               IntermediateSpacingType; 
-    typedef IntermediateImageType::IndexType                 IntermediateIndexType;
-    typedef IntermediateImageType::PointType                 IntermediatePointType;
+  typedef IntermediateImageType::Pointer                   IntermediateImagePointer;
+  typedef IntermediateImageType::RegionType                IntermediateRegionType;
+  typedef IntermediateImageType::SizeType                  IntermediateSizeType; 
+  typedef IntermediateImageType::SpacingType               IntermediateSpacingType; 
+  typedef IntermediateImageType::IndexType                 IntermediateIndexType;
+  typedef IntermediateImageType::PointType                 IntermediatePointType;
     
-    typedef OutputImageType::Pointer                         OutputImagePointer;
-    typedef OutputImageType::RegionType                      OutputRegionType;
-    typedef OutputImageType::SizeType                        OutputSizeType;
-    typedef OutputImageType::SpacingType                     OutputSpacingType;
+  typedef OutputImageType::Pointer                         OutputImagePointer;
+  typedef OutputImageType::RegionType                      OutputRegionType;
+  typedef OutputImageType::SizeType                        OutputSizeType;
+  typedef OutputImageType::SpacingType                     OutputSpacingType;
     
-    /** Set/Get the delineation ratio defined as the ratio of the delineated slices in the 
-     * output image (i.e. after interpolation) to the number of the delineated slices in 
-     * the input image (i.e. before interpolation). */
-    itkSetMacro( DelineationRatio, unsigned int );
-    itkGetConstReferenceMacro( DelineationRatio, unsigned int ); 
+  /** Set/Get the delineation ratio defined as the ratio of the
+    * delineated slices in the output image (i.e. after interpolation) to
+    * the number of the delineated slices in the input image (i.e. before
+    * interpolation)*/
+  itkSetMacro( DelineationRatio, unsigned int );
+  itkGetConstReferenceMacro( DelineationRatio, unsigned int ); 
     
-    itkStaticConstMacro( ImageDimension, unsigned int, 3 );
+  itkStaticConstMacro( ImageDimension, unsigned int, 3 );
     
-    typedef itk::InterpolateImageFunction< IntermediateImageType2, double>     InterpolatorType;
+  typedef itk::InterpolateImageFunction< IntermediateImageType2, double>     InterpolatorType;
     
-    /** Set the interpolator function.  The default is
+  /** Set the interpolator function.  The default is
     * itk::LinearInterpolateImageFunction. Some
     * other options are itk::NearestNeighborInterpolateImageFunction
     * and itk::BSplineInterpolateImageFunction
     * (which provides a higher order of interpolation).  */
-    itkSetObjectMacro( Interpolator, InterpolatorType );
+  itkSetObjectMacro( Interpolator, InterpolatorType );
     
-    /** Get a pointer to the interpolator function. */
-    itkGetConstObjectMacro( Interpolator, InterpolatorType );
-       
+  /** Get a pointer to the interpolator function. */
+  itkGetConstObjectMacro( Interpolator, InterpolatorType );
     
-    
-  protected:
-    /** Default constructor and destructor. */
-    IntraBinaryShapeBasedInterpolationImageFilter();
-    ~IntraBinaryShapeBasedInterpolationImageFilter();
+protected:
+  /** Default constructor and destructor. */
+  IntraBinaryShapeBasedInterpolationImageFilter();
+  ~IntraBinaryShapeBasedInterpolationImageFilter();
         
-    // Override since the output image has a different size as that of the input
-    void GenerateOutputInformation();
+  // Override since the output image has a different size as that of the input
+  void GenerateOutputInformation();
     
-    // Override since the filter requests a larger requested region on the input
-    void GenerateInputRequestedRegion();
+  // Override since the filter requests a larger requested region on the input
+  void GenerateInputRequestedRegion();
 
-    void GenerateData();
+  void GenerateData();
     
-    // Find all slices that are delineated (i.e. wherein there exist non-zero pixel intensities). 
-    // They must be joint/contiguous in the input image.
-    // The distance map calculation will be performed on these slices only to gain time of calculation.
-    void FindZCoordinatesOfDelineatedSlices();
+  // Find all slices that are delineated (i.e. wherein there exist
+  // non-zero pixel intensities). 
+  // They must be joint/contiguous in the input image.
+  // The distance map calculation will be performed on these slices
+  // only to gain time of calculation.
+  void FindZCoordinatesOfDelineatedSlices();
     
-    // Generate the information (origin, spacing, size, LagestPossibleRegion,...) of the image made of the delineated slices only
-    void GenerateIntermediateImageInformation();
+  // Generate the information (origin, spacing, size,
+  // LagestPossibleRegion,...) of the image made of the delineated
+  // slices only 
+  void GenerateIntermediateImageInformation();
     
+  /** Display. */
+  void PrintSelf(std::ostream& os, Indent indent) const;
     
-    /** Display. */
-    void PrintSelf(std::ostream& os, Indent indent) const;
+  typedef itk::SliceBySliceImageFilter< InputImageType, IntermediateImageType2 >                      SliceBySliceFilterType;
+  typedef itk::SignedDanielssonDistanceMapImageFilter< InternalInImageType, InternalOutImageType >    DistanceMapImageFilterType;   
+  typedef itk::ResampleImageFilter< IntermediateImageType2, IntermediateImageType2 >                  ResampleFilterType;
+  typedef itk::IdentityTransform< double, 3 >                                                         TransformType;
     
-    typedef itk::SliceBySliceImageFilter< InputImageType, IntermediateImageType2 >                      SliceBySliceFilterType;
-    typedef itk::SignedDanielssonDistanceMapImageFilter< InternalInImageType, InternalOutImageType >    DistanceMapImageFilterType;   
-    typedef itk::ResampleImageFilter< IntermediateImageType2, IntermediateImageType2 >                  ResampleFilterType;
-    typedef itk::IdentityTransform< double, 3 >                                                         TransformType;
-    
-    SliceBySliceFilterType::Pointer                                                            m_SliceBySliceFilter;
-    DistanceMapImageFilterType::Pointer                                                        m_DistanceMapImageFilter;
-    ResampleFilterType::Pointer                                                                m_ResampleFilter;
-    TransformType::Pointer                                                                     m_Transform;
+  SliceBySliceFilterType::Pointer                                                            m_SliceBySliceFilter;
+  DistanceMapImageFilterType::Pointer                                                        m_DistanceMapImageFilter;
+  ResampleFilterType::Pointer                                                                m_ResampleFilter;
+  TransformType::Pointer                                                                     m_Transform;
 
-  private:
-    //The copy constructor and assignment operator should be declared private and
-    //not implemented, to prevent instantiating the ﬁlter without the factory methods
-    IntraBinaryShapeBasedInterpolationImageFilter( const Self& );  // intentionally not implemented
-    void operator=( const Self& );                                 // intentionally not implemented
+private:
+  //The copy constructor and assignment operator should be declared
+  // private and not implemented, to prevent instantiating the ﬁlter
+  // without the factory methods
+  IntraBinaryShapeBasedInterpolationImageFilter( const Self& );  // intentionally not implemented
+  void operator=( const Self& );                                 // intentionally not implemented
     
-    unsigned int                            m_DelineationRatio;    // defined as the ratio of the delineated slices in the output 
-                                                                   // image (i.e. after interpolation) to the number of the 
-                                                                   // delineated slices in the input image (i.e. before interpolation)
-    std::vector< int >                      m_DelineationZCoordinateArray;
-    IntermediateImagePointer                m_IntermediateImage;
-    InterpolatorType::Pointer      m_Interpolator;
-  };
+  unsigned int                            m_DelineationRatio;    // defined as the ratio of the delineated slices in the output 
+  // image (i.e. after interpolation) to the number of the 
+  // delineated slices in the input image (i.e. before interpolation)
+  std::vector< int >                      m_DelineationZCoordinateArray;
+  IntermediateImagePointer                m_IntermediateImage;
+  InterpolatorType::Pointer      m_Interpolator;
+};
 } // end namespace itk
-#endif  // #define __INTRABINARYSHAPEBASEDINTERPOLATIONIMAGEFILTER_H
+#endif  // #define __itkIntraBinaryShapeBasedInterpolationImageFilter_h
