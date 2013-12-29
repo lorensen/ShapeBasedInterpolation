f80a6e2a2894c3a3ccf2974aba29c9b15d2ae7e4
Bill Lorensen <bill.lorensen@gmail.com>
2013-12-29 14:47:09 -0500

    ENH: Provide ITK remote access.

---
+++ ShapeBasedInterpolation.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(ShapeBasedInterpolation
+  "A class to shape-based interpolation of binary images."
+  GIT_REPOSITORY https://github.com/lorensen/ShapeBasedInterpolation.git
+  GIT_TAG master
+  )
